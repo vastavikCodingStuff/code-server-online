2462acf00935fd534c73fa09d2f693dc6ba44e2d
Parth Vastavik <131694386+parthasdey2304@users.noreply.github.com>
2026-09-24 00:38:49 +0530

    chore: bump version to 1.3.0 for In-App Updater release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vastavik.codeauth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
